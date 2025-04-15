f43d50c3ab93ead68aa1dd384a051573ca87f881
Grace Chen Abudi <cheezumcali23@gmail.com>
2025-04-15 23:37:23 +0300

    feat/update: Update basic UI with game board and room form (#7)

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,9 +1,13 @@
-import GameBoard from "../components/GameBoard";
-import RoomForm from "../components/RoomForm";
+import RoomForm from "@client/components/GameBoard";
+import GameBoard from "@client/components/RoomForm";
 
 export default function HomePage() {
   return (
-    <main className="p-4">
+    // <main className="min-h-screen p-4 flex flex-col items-center justify-start">
+    <main className="min-h-screen p-4">
+      <h1 className="text-4xl font-bold mt-8 flex justify-center">
+        Tic Tac Toe
+      </h1>
       <RoomForm />
       <GameBoard />
     </main>
